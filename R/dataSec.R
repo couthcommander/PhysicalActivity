c2076193a8380c42ceca95548e183d67cb29336f
beckca <cole.beck@vanderbilt.edu>
2017-01-03 12:06:47 -0600

    restore dataSec doc

--- R/dataSec.R
+++ R/dataSec.R
@@ -2,17 +2,17 @@
 #'
 #' Approximately 3 days of accelerometer data collected with 1-sec epoch in the
 #' correct data format that can be used by \code{wearingMarking} to classify
-#' wear and nonwear time.  The variables are as follows:
+#' wear and nonwear time.
 #'
-#' \itemize{
-#'   \item TimeStamp. a character vector
-#'   \item counts. a numeric vector
+#' @format A data frame with 238140 observations on the following 2 required variables.
+#' \describe{
+#'   \item{TimeStamp}{a character vector}
+#'   \item{counts}{a numeric vector}
 #' }
 #'
 #' @template ref2010
 #'
 #' @keywords datasets
-#' @format A data frame with 238140 observations and 2 variables.
 #'
 #' @examples
 #' data(dataSec)
