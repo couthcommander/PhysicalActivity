6749a90b156d9f40f8323355aeb0bb220aa3a134
choileena <naturechoi@gmail.com>
2018-05-03 11:03:07 -0500

    Update queryActigraph.R

--- R/queryActigraph.R
+++ R/queryActigraph.R
@@ -1,13 +1,13 @@
 #' Query ActiGraph File
 #'
-#' This function executes a SELECT query on an ActiGraph agd file.
+#' This function executes a SELECT query on an ActiGraph AGD file.
 #'
-#' agd files are actually SQLite databases.  This function requires the
+#' AGD files are actually SQLite databases.  This function requires the
 #' \code{RSQLite} package.  The user is encouraged to directly interface with
 #' the database by creating a connection with the \code{DBI} package.  This has
-#' been tested with agd files produced with ActiLife v6.11.
+#' been tested with AGD files produced with ActiLife v6.11.
 #'
-#' @param datfile An agd file.
+#' @param datfile An AGD file.
 #' @param qry An SQL SELECT statement.
 #'
 #' @return data.frame with query results.
