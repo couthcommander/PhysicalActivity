89a1790ff83aa0e8e7c97a53c4f6697b5b0e4d78
choileena <naturechoi@gmail.com>
2018-05-03 10:53:06 -0500

    Update deliveryData.R

--- R/deliveryData.R
+++ R/deliveryData.R
@@ -4,12 +4,12 @@
 #'
 #' @format A data frame with 20987 observations on the following variables.
 #' \describe{
-#'   \item{TimeStamp}{a character vector, timestamp of accelerometer measurements}
-#'   \item{axis1}{a numeric vector, counts from axis1 of 3-axis accelerometer}
-#'   \item{axis2}{a numeric vector, counts from axis2 of 3-axis accelerometer}
-#'   \item{axis3}{a numeric vector, counts from axis3 of 3-axis accelerometer}
-#'   \item{steps}{a numeric vector, the number of steps}
-#'   \item{vm}{a numeric vector, the vector magnitude calculated from counts of axis1, axis2 and axis3}
+#'   \item{TimeStamp}{A character vector, timestamp of accelerometer measurements}
+#'   \item{axis1}{A numeric vector, counts from axis1 of 3-axis accelerometer}
+#'   \item{axis2}{A numeric vector, counts from axis2 of 3-axis accelerometer}
+#'   \item{axis3}{A numeric vector, counts from axis3 of 3-axis accelerometer}
+#'   \item{steps}{A numeric vector, the number of steps}
+#'   \item{vm}{A numeric vector, the vector magnitude calculated from counts of axis1, axis2 and axis3}
 #' }
 #'
 #' @keywords datasets
